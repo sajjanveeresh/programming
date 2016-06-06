4fe2967b9f24d08fe4e6e69b02b53e9887d51ea6
Veeresh Sajjan <sajjan045@gmail.com>
2016-06-06 14:44:55 +0530

    assignement pgms

--- bottles_rhyme.rb
+++ bottles_rhyme.rb
@@ -3,9 +3,16 @@
 
 a = 99
 while a != 0
-	puts "#{a}" " bottles of beer on the wall" " #{a}" "bottles of beer"
+	if a == 1
+		puts "#{a}" " bottle of beer on the wall" " #{a}" " bottle of beer"
+	else
+		puts "#{a}" " bottles of beer on the wall" " #{a}" " bottles of beer"
+	end
+
 	a = a-1
-	if a > 0
+	if a == 1
+		puts "Take one down, pass it around," "#{a}" " bottle of beer on the wall..."
+	elsif a == 0
 		puts "Take one down, pass it around," "#{a}" " bottles of beer on the wall..."
 	else
 		puts "Take one down, pass it around, no more bottles of beer on the wall..."
